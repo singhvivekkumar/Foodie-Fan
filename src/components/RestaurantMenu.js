9fc1493167648545c4ee85760e27d8ee87ff24ff
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-06-10 21:47:39 +0530

    design restaurnat page

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,28 +1,35 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { RESTAURANT_ID_URL } from "../config";
+import { IMG_CDN_URL, RESTAURANT_ID_URL } from "../config";
+import ShimmerUI from "./ShimmerUI";
 
 const RestaurantMenu = () => {
 	const {id} = useParams();
-	const [restaurant, setRestaurant] = useState();
+	const [restaurant, setRestaurant] = useState({});
 
 	useEffect( ()=> {
 		getRestaurantDetails();
 	}, []);
 
 	async function getRestaurantDetails() {
-		const data = await fetch(RESTAURANT_ID_URL);
+		const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.5940499&lng=85.1376051&restaurantId=${id}&submitAction=ENTER`);
 		const jsonData = await data.json();
-		setRestaurant(jsonData?.data);
-		console.log(jsonData?.data);
+		setRestaurant(jsonData?.data?.cards[0]?.card?.card.info);
+		console.log(jsonData?.data?.cards[0]?.card?.card.info);
 	}
 
+
 	
-	console.log("whole componentw")
-	return (
-		<div>
-			<h1>Restaurant id :{id}</h1>
-			<h3>Name of restaurant : {restaurant}</h3>
+	console.log("whole component")
+	return (!restaurant)? <ShimmerUI/> : (
+		<div className=" flex flex-col font-serif m-5 p-4">
+			<div className=" flex justify-evenly ">
+				<span className=" text-3xl font-semibold">{restaurant.name}</span>
+				<span className=" fa fa-star-half-empty ">{restaurant.avgRating}</span>
+				<img className=" h-52 w-80 " src={IMG_CDN_URL + restaurant.cloudinaryImageId} />
+			</div>
+			<div></div>
+			<div></div>
 		</div>
 	);
 }
